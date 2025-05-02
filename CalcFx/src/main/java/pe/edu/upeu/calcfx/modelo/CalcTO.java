ddc294999e8dd97677ce43a34b6f2994b826c673
anghelo523 <lucariotorresarias@gmail.com>
2025-05-02 12:05:47 -0500

    hola3

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/modelo/CalcTO.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/modelo/CalcTO.java
@@ -1,50 +1,21 @@
 package pe.edu.upeu.calcfx.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+@Entity
+@Data
 public class CalcTO {
-    public int getId() {
-        return id;
-    }
+    public class calcTO {
 
-    public void setId(int id) {
-        this.id = id;
-    }
+        @Id
+        long id;
+        String num1;
+        String num2;
+        char operador;
+        String resultado;
 
-    int id;
-    String num1;
-    String num2;
-    public String getNum2() {
-        return num2;
-    }
-
-    public void setNum2(String num2) {
-        this.num2 = num2;
-    }
-
-    public String getNum1() {
-        return num1;
-    }
-
-    public void setNum1(String num1) {
-        this.num1 = num1;
-    }
-
-    char operator;
-    String resultado;
-
-
-    public char getOperator() {
-        return operator;
-    }
-
-    public void setOperator(char operator) {
-        this.operator = operator;
-    }
-
-    public String getResultado() {
-        return resultado;
-    }
-
-    public void setResultado(String resultado) {
-        this.resultado = resultado;
     }
 }
